29e14b334d1b1fdf2cf2f00f757eabe8c18e8746
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2025-11-23 17:26:41 +0300

    resolved bugs and solved conflicts  in bankapplication

---
+++ src/test/java/com/example/bankapp/BankappApplication.java
@@ -0,0 +1,11 @@
+package com.example.bankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankappApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BankappApplication.class, args);
+    }
+}
